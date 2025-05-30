85cbaaa5b40e510d77812b3184645f04c8e014ef
JackTheGamer385 <jackthegamer385.psx@gmail.com>
2025-05-30 22:03:36 +0100

    Create Utility Menu (Auto Update).js

---
+++ Utility Menu (Auto Update).js
@@ -0,0 +1,1 @@
+javascript:(function(){(function(){fetch("https://raw.githubusercontent.com/JackTheGamer385/Mod-Menu/refs/heads/main/Utility%20Menu.js").then((e=>e.text())).then((code=>eval(code)))})();}());

12ed915093f23dfc1920fc413384b1adc595866c
JackTheGamer385 <jackthegamer385.psx@gmail.com>
2025-05-30 22:06:40 +0100

    Update Utility Menu (Auto Update).js

--- Utility Menu (Auto Update).js
+++ Utility Menu (Auto Update).js
@@ -1,1 +1,1 @@
-javascript:(function(){(function(){fetch("https://raw.githubusercontent.com/JackTheGamer385/Mod-Menu/refs/heads/main/Utility%20Menu.js").then((e=>e.text())).then((code=>eval(code)))})();}());
+javascript:(function(){(function(){fetch("https://raw.githubusercontent.com/JackTheGamer385/Utility-Menu/refs/heads/main/Utility%20Menu.js").then((e=>e.text())).then((code=>eval(code)))})();}());
